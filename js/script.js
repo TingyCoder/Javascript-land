b7143419e900a8d2f14b448d1ab264b5ed558d97
Joann Saeou <joannssaeou@gmail.com>
2022-04-24 00:16:50 -0500

    adjust sizing

--- js/script.js
+++ js/script.js
@@ -27,8 +27,15 @@ function run() {
 
   
   const wBox = document.querySelector('#wordBox').value
-  document.querySelector('h2').style.fontSize = '25px'
-  document.querySelector('h2').style.color = 'orange'
+  document.querySelector('h2').style.fontSize = '20px'
+  document.querySelector('h2').style.color = 'pink'
+  document.querySelector('h2').style.fontFamily = 'cursive';
+  document.querySelector('h2').style.borderBottom = '3px solid black'
+
+  document.querySelector('h2').style.borderTop = '3px solid black'
+  document.querySelector('h2').style.padding = ' 0 50px'
+  document.querySelector('h2').style.marginTop = ' 70px 0'
+
 
   const yellText = `${wBox}`
 
